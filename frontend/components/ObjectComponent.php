8a2206517f08480f875c8f5222aebd9dfd3fd02a
zhandos38 <zhandos38@gmail.com>
2019-11-25 11:50:19 +0600

    Object shift fixes

--- frontend/components/ObjectComponent.php
+++ frontend/components/ObjectComponent.php
@@ -17,10 +17,6 @@ class ObjectComponent extends Component
             ->where(['user_id' => \Yii::$app->user->identity->getId()])
             ->one();
 
-        if (empty($lastShift)) {
-            return $this->createShift();
-        }
-
         if ($lastShift->status == ShiftHistory::STATUS_CLOSED) {
             \Yii::$app->session->set('shift_id', null);
             return false;
