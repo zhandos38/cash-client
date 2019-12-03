a5d83df9480ea426f700951b1d81fa42f83e3951
dias1995 <d.kozhanov@smartideagroup.kz>
2019-12-03 17:21:16 +0600

    fix

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -15,7 +15,7 @@ MenuAsset::register($this);
 <a href="<?= Url::to('/site/logout') ?>" data-method="post" class="back-button"><i class="fa fa-reply-all" aria-hidden="true"></i> Выйти (<?php  if (!Yii::$app->user->isGuest) echo Yii::$app->user->identity->username;?>)</a>
 <div id="menu-app" class="site-index">
     <div class="row">
-        <div class="col-xs-12 col-sm-4 col-md-4">
+        <div class="col-xs-12 col-sm-6 col-md-3">
             <a class="admin-block" href="<?= Url::to('/site/edit-profile') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -27,7 +27,7 @@ MenuAsset::register($this);
                 </div>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-4" style="<?= Yii::$app->user->can('manageStaff') ? '' : 'display: none' ?>">
+        <div class="col-xs-6 col-sm-6 col-md-3" style="<?= Yii::$app->user->can('manageStaff') ? '' : 'display: none' ?>">
             <a class="admin-block" href="<?= Url::to('/staff/main') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -39,7 +39,7 @@ MenuAsset::register($this);
                 </div>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-4" style="<?= Yii::$app->user->can('manageCustomer') ? '' : 'display: none' ?>">
+        <div class="col-xs-6 col-sm-6 col-md-3" style="<?= Yii::$app->user->can('manageCustomer') ? '' : 'display: none' ?>">
             <a class="admin-block" href="<?= Url::to('/customer/main') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -51,7 +51,7 @@ MenuAsset::register($this);
                 </div>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-4" style="<?= Yii::$app->user->can('manageWarehouse') ? '' : 'display: none' ?>">
+        <div class="col-xs-6 col-sm-6 col-md-3" style="<?= Yii::$app->user->can('manageWarehouse') ? '' : 'display: none' ?>">
             <a class="admin-block" href="<?= Url::to('/product/main') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -63,7 +63,7 @@ MenuAsset::register($this);
                 </div>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-4" style="<?= Yii::$app->user->can('manageOrder') ? '' : 'display: none' ?>">
+        <div class="col-xs-6 col-sm-6 col-md-3" style="<?= Yii::$app->user->can('manageOrder') ? '' : 'display: none' ?>">
             <a class="admin-block" href="<?= Url::to('/order/index') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -75,7 +75,7 @@ MenuAsset::register($this);
                 </div>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-4" style="<?= Yii::$app->user->can('manageInvoice') ? '' : 'display: none' ?>">
+        <div class="col-xs-6 col-sm-6 col-md-3" style="<?= Yii::$app->user->can('manageInvoice') ? '' : 'display: none' ?>">
             <a class="admin-block" href="<?= Url::to('/invoice/main') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -87,7 +87,7 @@ MenuAsset::register($this);
                 </div>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-4" style="<?= Yii::$app->user->can('manageSupplier') ? '' : 'display: none' ?>">
+        <div class="col-xs-6 col-sm-6 col-md-3" style="<?= Yii::$app->user->can('manageSupplier') ? '' : 'display: none' ?>">
             <a class="admin-block" href="<?= Url::to('/supplier/main') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -99,7 +99,7 @@ MenuAsset::register($this);
                 </div>
             </a>
         </div>
-        <div class="col-xs-6 col-sm-6 col-md-4" style="<?= Yii::$app->user->can('createOrder') ? '' : 'display: none' ?>">
+        <div class="col-xs-6 col-sm-6 col-md-3" style="<?= Yii::$app->user->can('createOrder') ? '' : 'display: none' ?>">
             <a class="admin-block" href="<?= Url::to('/order/test-create') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -112,7 +112,7 @@ MenuAsset::register($this);
             </a>
         </div>
         <?php if (Yii::$app->user->can('createOrder')): ?>
-            <div class="col-xs-6 col-sm-6 col-md-4" @click="openShiftOpenModal" v-show="openShiftActive">
+            <div class="col-xs-6 col-sm-6 col-md-3" @click="openShiftOpenModal" v-show="openShiftActive">
                 <a class="admin-block" href="javascript:void(0)">
                     <div class="admin-block__item">
                         <div class="admin-block__icon">
@@ -124,7 +124,7 @@ MenuAsset::register($this);
                     </div>
                 </a>
             </div>
-            <div class="col-xs-6 col-sm-6 col-md-4" @click="openShiftCloseModal" v-show="!openShiftActive">
+            <div class="col-xs-6 col-sm-6 col-md-3" @click="openShiftCloseModal" v-show="!openShiftActive">
                 <a class="admin-block" href="javascript:void(0)">
                     <div class="admin-block__item">
                         <div class="admin-block__icon">
@@ -137,7 +137,7 @@ MenuAsset::register($this);
                 </a>
             </div>
         <?php endif; ?>
-        <div class="col-xs-6 col-sm-6 col-md-4">
+        <div class="col-xs-6 col-sm-6 col-md-3">
             <a class="admin-block" href="<?= Url::to('/cash-draw/index') ?>">
                 <div class="admin-block__item">
                     <div class="admin-block__icon">
@@ -151,49 +151,52 @@ MenuAsset::register($this);
         </div>
     </div>
     <div class="shift-modal__wrapper" v-show="shiftCloseModalActive">
-        <div class="shift-modal">
+        <div class="shift-close-modal">
             <div class="comment-modal__close pull-right" @click="closeShiftCloseModal">
                 <i class="fas fa-times"></i>
             </div>
             <div class="shift-modal__container">
-                <div class="shift-modal__info">
-                    <div class="shift-modal__balance">{{ cashBoxBalance }}</div>
-                    <div class="shift-modal__cashier">
-                        <span>Кассир:</span> <?= Yii::$app->user->identity->full_name ?>
+                <h2>Закрыть смену</h2>
+                <div class="shift-close__modal-content">
+                    <div class="shift-modal__info">
+                        <div class="shift-modal__balance">{{ cashBoxBalance }}0.00</div>
+                        <div class="shift-modal__cashier">
+                            <p><span>Кассир:</span> <?= Yii::$app->user->identity->full_name ?></p>
+                        </div>
+                        <div class="shift-modal__date">
+                            <p><span>Дата/Время:</span> <label id="current-date">{{ currentTime }}</label></p>
+                        </div>
                     </div>
-                    <div class="shift-modal__date">
-                        <span>Дата/Время:</span> <span id="current-date">{{ currentTime }}</span>
+                    <div class="numpad-wrapper">
+                        <div class="numpad">
+                            <button type="button" class="numpad__button" @click="setBalance('1')">1</button>
+                            <button type="button" class="numpad__button" @click="setBalance('2')">2</button>
+                            <button type="button" class="numpad__button" @click="setBalance('3')">3</button>
+                            <button type="button" class="numpad__button" @click="setBalance('4')">4</button>
+                            <button type="button" class="numpad__button" @click="setBalance('5')">5</button>
+                            <button type="button" class="numpad__button" @click="setBalance('6')">6</button>
+                            <button type="button" class="numpad__button" @click="setBalance('7')">7</button>
+                            <button type="button" class="numpad__button" @click="setBalance('8')">8</button>
+                            <button type="button" class="numpad__button" @click="setBalance('9')">9</button>
+                            <button type="button" class="numpad__button" @click="setBalance('.')">,</button>
+                            <button type="button" class="numpad__button" @click="setBalance('0')">0</button>
+                            <button type="button" class="numpad__button" @click="cleanBalance">C</button>
+                        </div>
+                        <div class="numpad-extension">
+                            <button type="button" class="numpad-extension__button" @click="setBalance(0.5)">+0.5</button>
+                            <button type="button" class="numpad-extension__button" @click="setBalance(1)">+1</button>
+                            <button type="button" class="numpad-extension__button" @click="setBalance(2)">+2</button>
+                            <button type="button" class="numpad-extension__button" @click="setBalance(5)">+5</button>
+                            <button type="button" class="numpad-extension__button" @click="setBalance(10)">+10</button>
+                            <button type="button" class="numpad-extension__button" @click="setBalance(20)">+20</button>
+                            <button type="button" class="numpad-extension__button" @click="setBalance(50)">+50</button>
+                            <button type="button" class="numpad-extension__button" @click="setBalance(100)">+100</button>
+                        </div>
                     </div>
                 </div>
-                <div class="numpad-wrapper">
-                    <div class="numpad">
-                        <button type="button" class="numpad__button" @click="setBalance('1')">1</button>
-                        <button type="button" class="numpad__button" @click="setBalance('2')">2</button>
-                        <button type="button" class="numpad__button" @click="setBalance('3')">3</button>
-                        <button type="button" class="numpad__button" @click="setBalance('4')">4</button>
-                        <button type="button" class="numpad__button" @click="setBalance('5')">5</button>
-                        <button type="button" class="numpad__button" @click="setBalance('6')">6</button>
-                        <button type="button" class="numpad__button" @click="setBalance('7')">7</button>
-                        <button type="button" class="numpad__button" @click="setBalance('8')">8</button>
-                        <button type="button" class="numpad__button" @click="setBalance('9')">9</button>
-                        <button type="button" class="numpad__button" @click="setBalance('.')">,</button>
-                        <button type="button" class="numpad__button" @click="setBalance('0')">0</button>
-                        <button type="button" class="numpad__button" @click="cleanBalance">C</button>
-                    </div>
-                    <div class="numpad-extension">
-                        <button type="button" class="numpad-extension__button" @click="setBalance(0.5)">+0.5</button>
-                        <button type="button" class="numpad-extension__button" @click="setBalance(1)">+1</button>
-                        <button type="button" class="numpad-extension__button" @click="setBalance(2)">+2</button>
-                        <button type="button" class="numpad-extension__button" @click="setBalance(5)">+5</button>
-                        <button type="button" class="numpad-extension__button" @click="setBalance(10)">+10</button>
-                        <button type="button" class="numpad-extension__button" @click="setBalance(20)">+20</button>
-                        <button type="button" class="numpad-extension__button" @click="setBalance(50)">+50</button>
-                        <button type="button" class="numpad-extension__button" @click="setBalance(100)">+100</button>
-                    </div>
+                <div class="shift-modal-close__btn" @click="closeShift">
+                    Закрыть смену
                 </div>
-            </div>
-            <div class="shift-modal__btn" @click="closeShift">
-                Закрыть смену
             </div>
         </div>
     </div>
@@ -205,16 +208,16 @@ MenuAsset::register($this);
             <div class="shift-modal__container">
                 <h2>Открыть смену</h2>
                 <div class="shift-modal__cashier">
-                    <span>Кассир:</span> <?= Yii::$app->user->identity->full_name ?>
+                    <p><span>Кассир:</span> <?= Yii::$app->user->identity->full_name ?></p>
                 </div>
                 <div class="shift-modal__date">
-                    <span>Дата/Время:</span> <span id="current-date">{{ currentTime }}</span>
+                    <p><span>Дата/Время:</span> <label id="current-date">{{ currentTime }}</label></p>
                 </div>
                 <div class="shift-modal__warning">
-                    Пожалуйста, сверьте время
+                    <p>Пожалуйста, сверьте время</p>
                 </div>
                 <div class="shift-modal__btn" @click="setShift">
-                    Открыть смену
+                    <a href="">Открыть смену</a>
                 </div>
             </div>
         </div>
